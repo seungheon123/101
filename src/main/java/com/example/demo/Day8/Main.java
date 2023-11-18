157910ec1a57b9e2f384df8cb62a75de772ac670
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-11-19 00:04:23 +0900

    백준 17219번

--- src/main/java/com/example/demo/Day8/Main.java
+++ src/main/java/com/example/demo/Day8/Main.java
@@ -1,8 +1,6 @@
 package com.example.demo.Day8;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
