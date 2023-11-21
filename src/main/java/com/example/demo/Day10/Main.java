7350cb1f79c33bbfb0528c4a3fb3306cd7ccef3c
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-11-21 16:37:35 +0900

    백준 15903번

---
+++ src/main/java/com/example/demo/Day10/Main.java
@@ -0,0 +1,34 @@
+package com.example.demo.Day10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        long res = 0;
+        long[] arr = new long[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i =0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        while(m-->0){
+            Arrays.sort(arr);
+            long sum = arr[0] + arr[1];
+            arr[0] = sum;
+            arr[1] = sum;
+        }
+        for(long num : arr){
+            res+=num;
+        }
+        System.out.println(res);
+    }
+}
