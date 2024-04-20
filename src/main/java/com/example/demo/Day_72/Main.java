d3fbe57e2b3e524cf19026c398c26d834929836e
seungheon123 <seungheon.bisb.2012@gmail.com>
2024-04-21 01:25:09 +0900

    백준 16967번 배열 복원하기

--- src/main/java/com/example/demo/Day_72/Main.java
+++ src/main/java/com/example/demo/Day_72/Main.java
@@ -25,12 +25,13 @@ public class Main {
                 score[i][0] = Integer.parseInt(st.nextToken());
                 score[i][1] = Integer.parseInt(st.nextToken());
             }
-            Arrays.sort(score, new Comparator<int[]>() {
-                @Override
-                public int compare(int[] o1, int[] o2) {
-                    return Integer.compare(o1[0], o2[0]);
-                }
-            });
+//            Arrays.sort(score, new Comparator<int[]>() {
+//                @Override
+//                public int compare(int[] o1, int[] o2) {
+//                    return Integer.compare(o1[0], o2[0]);
+//                }
+//            });
+            Arrays.sort(score, (o1,o2)-> Integer.compare(o1[0],o2[0]));
             cnt = 1;
             int min = score[0][1];
             for(int i = 1; i<N; i++){
